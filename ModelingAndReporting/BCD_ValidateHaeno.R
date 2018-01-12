dde446e869fa399c63072699f8454e5747b3225f
Ewa Szczurek <31432967+EwaSzczurek@users.noreply.github.com>
2018-01-12 16:02:26 +0100

    Update BCD_ValidateHaeno.R
    cleaner code, comments

--- ModelingAndReporting/BCD_ValidateHaeno.R
+++ ModelingAndReporting/BCD_ValidateHaeno.R
@@ -1,6 +1,9 @@
+#################
+# Implements independent validation of model predictions on pancreatic cancer patients.
+#################
+
 source("A_Handy.R")
-source("BCD_Models.R")
-source("BCD_DataAccess.R")
+
 library(survival)
 lower= 2.5
 upper = 3.4
@@ -71,8 +74,6 @@ km.adjuvant <- survfit(Surv(Survival, Status) ~ 1, data = h.adjuvant, conf.type 
 h.km = rbind(h.autopsy, h.adjuvant)
 km<- survfit(Surv(Survival, Status) ~ Cohort, data = h.km)
 
-### km$surv
-### km$time
 
 ##### Our approximation of the survival functions from the quantiles
 load(paste(fitFolder,"Res_PancreasAdenoCa.RData",sep=""))
@@ -87,8 +88,6 @@ pars = getPars(res.exp, pars.basic)
 ### For every d, retrieve the quantiles
 ### appr is a matrix with quantile survival for every d, with ds as rows and quants as colums
 
-
-#h = h.adjuvant
 nam.ad = "adjuvant"
 nam.au = "autopsy"
 ds.adjuvant = h.adjuvant$SizePrimary
@@ -96,29 +95,6 @@ ds.autopsy = h.autopsy$SizePrimary
 appr.d.au = CollectQuantiles(quants, ds.autopsy, pars)
 appr.d.ad = CollectQuantiles(quants, ds.adjuvant, pars)
 
-
-### Now make a plot of the survival function and of the 1-quantiles
-
-
-
-doPlot<- function(tit, appr.d, km, haeno){
-
-	plot( x = km$time, y = km$surv,  ylab="Surviving fraction", xlab = "Years", col = "black",ylim=c(0,1),xlim=c(0,xm),  main=tit, type = "o", cex = 0.4, pch = 20)	
-	lines( x = appr.d, y = (1-as.numeric(names(appr.d))),col = "red")
-	lines( x = haeno, y = (1-as.numeric(names(haeno))),col = "orange", type = "o", cex = 0.5)
-	lines( x = km.adjuvant.b$time, y  = km.adjuvant.b$surv, col = "gray", type = "o", cex = 0.4)
-}
-
-printPlot<- function(nam, appr.d, km, haeno){
-	pdf(paste( "../HaenoData/HaenoValidationSurvival_",nam,"_v1.pdf",sep=""), width = 2.5, height = 2.6)
-	par(ps=9, mar=c(2.6, 2.7,2.3,0.2), mgp=c(1.6,0.4,0))
-	doPlot(nam, appr.d, km, haeno)
-	legend("topright",legend=c("Data","MFB","Haeno et al."),fill = c("black","red","orange"), cex = 0.7)
-	dev.off()
-}
-
-#printPlot(nam.ad, appr.d.ad, km.adjuvant, haeno.ad)
-#printPlot(nam.au, appr.d.au, km.autopsy, haeno.au)
 
 km.ad = data.frame(Years = km.adjuvant$time, Survival = km.adjuvant$surv, lci = km.adjuvant$lower, uci = km.adjuvant$upper, Cohort = rep("Adjuvant", length(km.adjuvant$time)))
 km.au = data.frame(Years = km.autopsy$time, Survival = km.autopsy$surv, lci = km.autopsy$lower, uci = km.autopsy$upper, Cohort = rep("Autopsy", length(km.autopsy$time)))
